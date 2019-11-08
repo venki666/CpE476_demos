3db4478a38e831fb864171debbcc894d5640eab9
venki666 <vm@unlv.nevada.edu>
2019-11-08 08:35:24 -0800

    OpenCV Tutorials

---
+++ OpenCV_Tutorials/simple/CMakeFiles/ImageLoads.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImageLoads.dir/imgLoad.cpp.o"
+  "ImageLoads.pdb"
+  "ImageLoads"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImageLoads.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
